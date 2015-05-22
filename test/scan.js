e0a330fabe337c4ad0d8568e92b8d296852c1e46
Jan Krems <jan.krems@groupon.com>
2015-05-21 19:57:32 -0700

    Support for scanning classes

--- test/scan.js
+++ test/scan.js
@@ -17,8 +17,8 @@ function Other() {
 test('scan a function without annotations', function(t) {
   function f() {}
 
-  const annotations = Annotation.scan(f);
-  t.deepEqual(annotations, [], 'No annotations expected');
+  const results = Annotation.scan(f);
+  t.deepEqual(results, [], 'No annotations expected');
 
   t.end();
 });
@@ -28,9 +28,11 @@ test('scan a function with annotation', function(t) {
   Simple(f);
   function f() {}
 
-  const annotations = Annotation.scan(f);
-  t.ok(annotations[0] instanceof Other, 'Other is found');
-  t.ok(annotations[1] instanceof Simple, 'Simple is found');
+  const results = Annotation.scan(f);
+  t.ok(results[0].annotation instanceof Other, 'Other is found');
+  t.ok(results[1].annotation instanceof Simple, 'Simple is found');
+  t.equal(results[0].target, f, 'Returns the target');
+  t.equal(results[1].target, f, 'Returns the target');
 
   t.end();
 });
@@ -40,9 +42,52 @@ test('scan a function, filtered by type', function(t) {
   Simple(f);
   function f() {}
 
-  const annotations = Annotation.scan(f, Simple);
-  t.equal(annotations.length, 1, 'Finds only one annotation');
-  t.ok(annotations[0] instanceof Simple, 'Finds the right annotation');
+  const results = Annotation.scan(f, Simple);
+  t.equal(results.length, 1, 'Finds only one annotation');
+  t.ok(results[0].annotation instanceof Simple, 'Finds the right annotation');
+  t.equal(results[0].target, f, 'Returns the target');
+
+  t.end();
+});
+
+test('scan a class', function(t) {
+  class Base {
+    a() {} // overridden
+    b() {} // visible
+  }
+  Simple(Base);
+  Simple(Base.prototype.a);
+  Simple(Base.prototype.b);
+  Other(Base.prototype.b);
+
+  class Derived extends Base {
+    a() {}
+    c() {} // only in derived
+  }
+  Other(Derived);
+  Simple(Derived);
+  Simple(Derived.prototype.a);
+  Simple(Derived.prototype.c);
+
+  const results = Annotation.scan(Derived, Simple);
+  t.equal(results.length, 4, '1 on class + 3 on methods = 4 found');
+  t.equal(results[0].target, Derived, 'Class itself comes first');
+  t.equal(results[1].target, Base.prototype.b, 'Then Base method b');
+  t.equal(results[2].target, Derived.prototype.a, 'Then Derived method a');
+  t.equal(results[3].target, Derived.prototype.c, 'Then Derived method c');
+
+  t.equal(results[1].ctor, Derived, 'Includes constructor for methods (b)');
+  t.equal(results[1].key, 'b', 'Includes property key for methods (b)');
+
+  t.equal(results[2].ctor, Derived, 'Includes constructor for methods (a)');
+  t.equal(results[2].key, 'a', 'Includes property key for methods (a)');
+
+  t.equal(results[3].ctor, Derived, 'Includes constructor for methods (c)');
+  t.equal(results[3].key, 'c', 'Includes property key for methods (c)');
+
+  results.forEach(function(r) {
+    t.ok(r.annotation instanceof Simple, 'Only returns Simple annotations');
+  });
 
   t.end();
 });
